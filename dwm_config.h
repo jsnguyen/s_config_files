370ad008eb8a3505c6038a4bd669e5cfc54e947b
Jayke Nguyen <jsn.phpro1701@gmail.com>
2019-05-14 22:22:26 -0700

    changed gimp

--- dwm_config.h
+++ dwm_config.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "EMPTY",     NULL,       NULL,       0,            0,           -1 },
 };
 
 /* layout(s) */
